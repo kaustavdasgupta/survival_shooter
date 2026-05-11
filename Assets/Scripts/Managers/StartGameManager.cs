9afc1d886644d409ff475a640b61f2b06cd992aa
KaustavDG <kaustavdasgupta0@gmail.com>
2026-05-11 20:37:49 +0530

    added start game canvas for loading game

---
+++ Assets/Scripts/Managers/StartGameManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class StartGameManager : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    public Transform player;
+    public PlayerHealth playerHealth;
+    public Button continueButton;
+    private string savePath;
+
+    void Start()
+    {
+        savePath = Application.persistentDataPath + "/save.json";
+        Time.timeScale = 0f;
+        menuCanvas.SetActive(true);
+        continueButton.interactable = File.Exists(savePath);
+    }
+
+    public void NewGame()
+    {
+        Time.timeScale = 1f;
+        menuCanvas.SetActive(false);
+    }
+
+    public void ContinueGame()
+    {
+        if (File.Exists(savePath))
+        {
+            SaveSystem.LoadPlayer(player, playerHealth);
+            Time.timeScale = 1f;
+            menuCanvas.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("No Save Found");
+        }
+    }
+}
